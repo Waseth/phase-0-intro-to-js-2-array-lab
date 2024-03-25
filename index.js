acfe570a810939ec3a98fa1313b9199b08c0e248
Waseth <waseth.emmanuel@student.moringaschool.com>
2024-03-25 20:39:31 +0300

    createmyfile.md

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(){
+    let appendedcats = [...cats,"Broom"]
+    return appendedcats
+}
+
+function prependCat(){
+    let prependedcats = ["Arnold", ...cats]
+    return prependedcats
+}
+
+function removeLastCat(){
+    let removedlastcat = cats.slice(0, cats.length -1)
+    return removedlastcat
+}
+
+function removeFirstCat(){
+    let removedfirstcat = cats.slice(1)
+    return removedfirstcat
+}
+
